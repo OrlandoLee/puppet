f93f67607f4a2d5b2715e649cc2be452c3ca63d0
OrlandoLee <zongshengli@ucla.edu>
2014-09-20 12:51:51 -0700

    ol - ubuntu install script

---
+++ ubuntuRails.sh
@@ -0,0 +1,46 @@
+!#/bin/bash
+#command for installing:
+
+
+sudo apt-get update
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+
+#ruby
+wget http://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p451.tar.gz
+tar -xvzf ruby-2.0.0-p451.tar.gz
+cd ruby-2.0.0-p451 && ./configure && make && sudo make install
+ruby -v
+
+#rails
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs
+gem install rails -v 4.0.1
+rails -v
+sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
+sudo apt-get -y install nginx
+
+#mongo
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
+apt-get -y update
+apt-get -y install mongodb-10gen
+sudo bash ./mongo_install.bash
+
+
+#java and elasticsearch
+cd ~
+sudo apt-get update
+sudo apt-get install openjdk-7-jre-headless -y
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.tar.gz
+tar -xvzf elasticsearch-1.3.2.tar.gz
+cd elasticsearch-1.3.2 && bin/elasticsearch -d
+curl localhost:9200
+
+#memcached
+apt-get install libevent-dev -y 
+wget http://memcached.org/files/memcached-1.4.20.tar.gz
+tar -zxvf memcached-1.4.20.tar.gz
+cd memcached-1.4.20
+./configure && make && make test && sudo make install
