33b4a409a33f8f18c52f04b740bd064c38478bed
OrlandoLee <zongshengli@ucla.edu>
2014-10-13 23:31:53 -0700

    Update ubuntuRails.sh

--- ubuntuRails.sh
+++ ubuntuRails.sh
@@ -31,7 +31,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
 apt-get -y update
 apt-get -y install mongodb-10gen
-sudo bash ./mongo_install.bash
+/usr/bin/mongod --config /etc/mongodb.conf
 
 
 #java and elasticsearch
